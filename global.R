4a0fc045f07c57262ce7ba8fb61de0946f026a77
awt <abdoulayewtoure@gmail.com>
2025-09-16 21:14:18 +0200

    file to load and manipulate data before using

---
+++ global.R
@@ -0,0 +1,23 @@
+#######################################################
+#################### DATA MANAGEMENT #################
+######################################################
+
+############# LIBRARY IMPORTATION #####################
+library(shiny)
+library(readr)
+library(readxl)
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+
+
+
+
+############### DATA IMPORTATION #####################
+
+
+############## DATA MANIPULATION ####################
+
+
+
+############## FUNCTIONS ###########################
