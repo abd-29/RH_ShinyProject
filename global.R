b14a0b03d6d9138c62c7010b21a85b1cbb5c9eca
awt <abdoulayewtoure@gmail.com>
2025-10-18 13:59:09 +0200

    merger les tables et nettoyages

--- global.R
+++ global.R
@@ -16,11 +16,38 @@ library(bslib)
 
 
 ############### DATA IMPORTATION #####################
-salaires <- read_excel("data/RH_Salaries.xlsx")
-contrats <- read_excel("data/RH_Contrats.xlsx")
+Salaires <- read_excel("data/RH_Salaries.xlsx")
+Contrats <- read_excel("data/RH_Contrats.xlsx")
 
 ############## DATA MANIPULATION ####################
+
+# renommer quelques colonnes des tables salaires et contrats 
+# les noms de colonnes en minuscules
+
+Salaires <- Salaires %>%
+  rename(
+    id_salarie = `id_salarié`,
+    annee_naissance = `Année naissance`,
+    etat_civil = `Etat Civil`,
+    sexe = Sexe,
+    enfants = Enfants,
+    salaire = Salaire,
+    age = Age
+  )
+
+Contrats <- Contrats %>%
+  rename(
+    id_salarie = `id_salarié`,
+    duree_hebdo = `Durée hebdo`,
+    contrat = Contrat
+    
+  )
+
+# exclure les salaries qui n'ont pas de contrat
+data <- Salaires %>%
+  inner_join(Contrats, by = "id_salarie")
 
 
 
 ############## FUNCTIONS ###########################
+
